b34835341062da828753cd46877657883754ebd5
BerkayTmz <adilberkaytemiz@gmail.com>
2020-12-19 21:23:47 +0300

    motor kontrol edildi kamerayla

--- motorpwm.py
+++ motorpwm.py
@@ -2,21 +2,78 @@ import serial
 from detector import Detector
 import cv2
 
-serialPort = serial.Serial(port = "COM0", baudrate=115200,
+serialPort = serial.Serial(port = "/dev/ttyUSB0", baudrate=115200,
                            bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
 
 serialString = ""                           # Used to hold data coming over UART
+
+RF = 10
+RR = 15
+LF = 20
+LR = 25
 
 while(1):
    # Wait until there is data waiting in the serial buffer
    # Print the contents of the serial data
    # Tell the device connected over the serial port that we recevied the data!
    # The b at the beginning is used to indicate bytes!
-   serialPort.write(b"Selamunaleykum haci abi\r\n")   
    with Detector(0, (640, 360)) as d:
       target = None
       while True:
-         frame, target, center = d.detect(target)
+         frame, target, center_x, center_y = d.detect(target)
          cv2.imshow("hello", frame)
-         if center:
-            print("Center")
+
+         if center_x:
+            center_x = int((center_x + 1)*100)
+            center_y = int((center_y + 1)*100)
+            
+            if(center_y > 30):
+               if(center_x < 90): #turn left
+                  drive = bytearray()
+                  drive.append(LR)
+                  drive.append(100)
+                  serialPort.write(drive)
+
+                  drive = bytearray()
+                  drive.append(RF)
+                  drive.append(100)
+                  serialPort.write(drive)
+                  print(f"Center: ({center_x}, {center_y})") 
+
+
+               elif(center_x > 110 ): # turn right
+                  drive = bytearray()
+                  drive.append(LF)
+                  drive.append(100)
+                  serialPort.write(drive)
+
+                  drive = bytearray()
+                  drive.append(RR)
+                  drive.append(100)
+                  serialPort.write(drive)
+                  print(f"Center: ({center_x}, {center_y})") 
+
+               else:
+                  drive = bytearray()
+                  drive.append(LF)
+                  drive.append(100)
+                  serialPort.write(drive)
+
+                  drive = bytearray()
+                  drive.append(RF)
+                  drive.append(100)
+                  serialPort.write(drive)
+                  print(f"Center: ({center_x}, {center_y})") 
+         else:
+               
+            drive = bytearray()
+            drive.append(LR)
+            drive.append(0)
+            serialPort.write(drive)
+
+            drive = bytearray()
+            drive.append(RF)
+            drive.append(0)
+            serialPort.write(drive)
+            print(f"Center: ({center_x}, {center_y})") 
+
--- serialTest.py
+++ serialTest.py
@@ -1,16 +1,62 @@
 import serial
 import time
-serialPort = serial.Serial(port = "COM0", baudrate=115200,
+serialPort = serial.Serial(port = "/dev/ttyUSB0", baudrate=115200,
                            bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
 
 serialString = ""                           # Used to hold data coming over UART
-
+x = 0x00
+RF = 10
+RR = 15
+LF = 20
+LR = 25
 while(1):
    # Wait until there is data waiting in the serial buffer
    # Print the contents of the serial data
    # Tell the device connected over the serial port that we recevied the data!
    # The b at the beginning is used to indicate bytes!
-   x = 0x00
-   serialPort.write(x)
-   x = x+1
-   time.sleep(100)   
+    drive = bytearray()
+    drive.append(RF)
+    drive.append(200)
+    serialPort.write(drive)  
+    print("Center: RF") 
+
+    time.sleep(5)
+    drive = bytearray()
+    drive.append(RF)
+    drive.append(0)
+    serialPort.write(drive)  
+    print("Center: RR") 
+
+    drive = bytearray()
+    drive.append(RR)
+    drive.append(200)
+    serialPort.write(drive)  
+    print("Center: RR") 
+
+    time.sleep(5)
+    drive = bytearray()
+    drive.append(RR)
+    drive.append(0)
+    serialPort.write(drive)  
+    print("Center: RR") 
+    drive = bytearray()
+    drive.append(LF)
+    drive.append(200)
+    serialPort.write(drive)  
+    print("Center: LF") 
+
+
+    time.sleep(5)
+    drive = bytearray()
+    drive.append(LF)
+    drive.append(0)
+    serialPort.write(drive)  
+    print("Center: LF") 
+    drive = bytearray()
+    drive.append(LR)
+    drive.append(200)
+    serialPort.write(drive)  
+    print("Center: LR") 
+
+    time.sleep(5)
+
