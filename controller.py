c75ee3e1cd0a09542059a2fd1c24c88ae82e5c27
BerkayTmz <adilberkaytemiz@gmail.com>
2021-05-23 13:25:42 +0300

    renamed controller with some parameter tunings

--- controller.py
+++ controller.py
@@ -11,13 +11,16 @@ from enum import Enum, auto
 
 
 client = redis.Redis()
-pubsub = client.pubsub()
+# pubsub = client.pubsub()
+
+# Rush algorithm parameters
+rush_stop_distance = 550
 
 # search algorithm parameters
-TURN_BASE_TIME = 0.75           # seconds
+TURN_BASE_TIME = 0.50           # seconds
 FORWARD_BASE_TIME = 1           # seconds
 T = 1                           # unitless
-stop_distance = 700             # millimeters
+search_stop_distance = 700      # millimeters
 obstacle_avoided_dist = 1000    # millimeters
 
 # Codes to communicate with Arduino to drive individual motors to each direction
@@ -29,10 +32,10 @@ LEFT_MOTOR_REVERSE = 15
 
 # Pre-defined motor speeds
 # Values must be between 0-255, this value configures to PWM pulse width
-DRIVE_SPEED = 110
+DRIVE_SPEED = 145
 STOP = 0
-DRIVE_TURN_SPEED = 70
-TURN_SPEED = 100
+DRIVE_TURN_SPEED = 60
+TURN_SPEED = 140
 
 # Search algorithm state data [DON'T MODIFY]
 s_state_changed = True
@@ -146,29 +149,30 @@ def cooldown(cd: float = None):
         cooldown_until = time.time() + cd
 
 
-def nextState(mode: str):
+def nextState(mode: str, add_cooldown: bool = True, cooldown_amount: float = 1):
     if mode == "s":
         global s_state, s_state_changed, s_cntr
         s_state += 1
         if s_state > 3:
             s_state = 0
             s_cntr += 1
-        cooldown(1)
         s_state_changed = True
     elif mode == "o":
         global o_state, o_state_changed
         o_state += 1
-        if o_state > 6:
+        if o_state > 7:
             o_state = 0
-        cooldown(1)
         o_state_changed = True
+
+    if add_cooldown:
+        cooldown(cooldown_amount)
 
 
 def rush(center_x, center_y):
-    global stop_distance
+    global rush_stop_distance
 
     f = lidar_distance("f")
-    if f and f < stop_distance:
+    if (f and f < rush_stop_distance) or not center_x:
         robotDrive("STOP")
         return
     elif center_x:
@@ -185,20 +189,19 @@ def rush(center_x, center_y):
         # s_cntr = 1
         # s_start_time = time.time()
 
-        else:
-            if(center_x < 90):  # turn left
-                # Turn left, slower left motor with turing speed and right motor with normal driving speed
-                #robotDriveArduino(LEFT_MOTOR_FORWARD, TURN_SPEED, RIGHT_MOTOR_FORWARD, DRIVE_SPEED)
-                robotDrive("DRIVE_LEFT")
+        if(center_x < 80):  # turn left
+            # Turn left, slower left motor with turing speed and right motor with normal driving speed
+            #robotDriveArduino(LEFT_MOTOR_FORWARD, TURN_SPEED, RIGHT_MOTOR_FORWARD, DRIVE_SPEED)
+            robotDrive("DRIVE_LEFT")
 
-            elif(center_x > 110):  # turn right
-                # Turn right, slower right motor with turn speed and left motor with normal driving speed
-                #robotDriveArduino(LEFT_MOTOR_FORWARD, DRIVE_SPEED, RIGHT_MOTOR_FORWARD, TURN_SPEED)
-                robotDrive("DRIVE_RIGHT")
-            else:
-                # Drive forward, this condition is only happens when the
-                # robotDriveArduino(LEFT_MOTOR_FORWARD, DRIVE_SPEED, RIGHT_MOTOR_FORWARD, DRIVE_SPEED)
-                robotDrive("FORWARD")
+        elif(center_x > 120):  # turn right
+            # Turn right, slower right motor with turn speed and left motor with normal driving speed
+            #robotDriveArduino(LEFT_MOTOR_FORWARD, DRIVE_SPEED, RIGHT_MOTOR_FORWARD, TURN_SPEED)
+            robotDrive("DRIVE_RIGHT")
+        else:
+            # Drive forward, this condition is only happens when the
+            # robotDriveArduino(LEFT_MOTOR_FORWARD, DRIVE_SPEED, RIGHT_MOTOR_FORWARD, DRIVE_SPEED)
+            robotDrive("FORWARD")
 
 
 def obstacleAvoidance():
@@ -244,8 +247,14 @@ def obstacleAvoidance():
 
 # Araya state ekle default bir süre gitsin (1 metre götürcek kadar mesela)
 # x initialize et, yatay gittiğin yolu ölç [gerekirse]
-
     elif o_state == 3:
+        if time.time() < o_start_time + FORWARD_BASE_TIME*T:
+            robotDrive("FORWARD")
+        else:
+            nextState("o", False)
+            return
+
+    elif o_state == 4:
         r = lidar_distance("r")
         # and time.time() < o_start_time + searchtengelen:
         if r and r < obstacle_avoided_dist:
@@ -254,14 +263,14 @@ def obstacleAvoidance():
             nextState("o")
             return
 
-    elif o_state == 4:
+    elif o_state == 5:
         if time.time() < o_start_time + (TURN_BASE_TIME*T):
             robotDrive("TURN_RIGHT")
         else:
             nextState("o")
             return
 
-    elif o_state == 5:
+    elif o_state == 6:
         r = lidar_distance("r")
         # and time.time() < o_start_time + searchtengelen:
         if time.time() < o_start_time + y:
@@ -270,7 +279,7 @@ def obstacleAvoidance():
             nextState("o")
             return
 
-    elif o_state == 6:
+    elif o_state == 7:
         if time.time() < o_start_time + (TURN_BASE_TIME*T):
             robotDrive("TURN_LEFT")
         else:
@@ -281,10 +290,10 @@ def obstacleAvoidance():
 
 
 def search():
-    global s_state_changed, s_state, s_cntr, s_start_time, stop_distance, Mode
+    global s_state_changed, s_state, s_cntr, s_start_time, search_stop_distance, Mode
 
     f = lidar_distance("f")
-    if f and f < stop_distance:
+    if f and f < search_stop_distance:
         robotDrive("STOP")
         Mode = Mode.ObstacleAvoidance
         cooldown(1)
@@ -346,19 +355,22 @@ def controller(center_x, center_y):
 
 
 while True:
+
     center_x, center_y = client.get("center:x"), client.get("center:y")
 
     center_x = None if center_x is None else float(center_x)
     center_y = None if center_y is None else float(center_y)
 
-    if center_x is not None and center_y is None:
-        print(f"{center_x:0.3f} SICTIIIN             ", end="\r")
-    else:
-        print(f"{None}              ", end="\r")
+    print(str(Mode) + "                  ", end="\r")
 
-    if center_y is not None:
-        print(f"{center_y:0.3f}              ", end="\r")
-    else:
-        print(f"{None}              ", end="\r")
+    # if center_x is not None and center_y is None:
+    #     print(f"{center_x:0.3f} SICTIIIN             ", end="\r")
+    # else:
+    #     print(f"{None}              ", end="\r")
+
+    # if center_y is not None:
+    #     print(f"{center_y:0.3f}              ", end="\r")
+    # else:
+    #     print(f"{None}              ", end="\r")
 
     controller(center_x, center_y)
